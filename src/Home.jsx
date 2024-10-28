7715d70b04db607d4fce004c267a2c5eee019359
infernodragon456 <vighumandy420@gmail.com>
2024-10-28 18:59:04 +0530

    Add Not Found page

--- src/Home.jsx
+++ src/Home.jsx
@@ -192,7 +192,7 @@ const Home = () => {
         <div >
           <div  className="home-body">
             <div ref={starsRef} className="stars">
-              <Stars/>
+              <Stars number={700}/>
             </div>
             <div className="home-main-container">
               <div className="logo">
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,31 +2,32 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import "./NotFound.css"
 import sadPenguinImage from '../assets/sad-penguin.png';
-import FadeIn from '../components/stars';
+import Stars from '../components/stars';
+import notfound from '../assets/not-found.png'
+import logo from "../assets/coming-soon/logo-new.svg";
 
 const NotFound = () => {
   return (
-    <FadeIn duration={500}>
-      <div className="notfound">
-        <div className="container">
-          <h1>404 - Page Not Found</h1>
-          <img src={sadPenguinImage} alt="Sad Penguin" />
+    <>
+      <div className="notfound-body">
+        <div className="stars">
+          <Stars number={500}/>
         </div>
-        <h3
-          style={{
-            marginTop: "10px",
-            fontWeight: "300",
-            fontFamily: "Share Tech, sans-serif",
-          }}
-        >
-          Sorry, the page you're looking for seems to be missing or the link is
-          incorrect.
-        </h3>
-        <NavLink to="/">
-          <button className="back">Go Back to Home</button>
-        </NavLink>
+        <div className="notfound-main-container">
+          <div className="logo">
+            <img src={logo} alt="Logo" />
+          </div>
+          <div className="notfound-title">
+            <img src={notfound} alt="Not Found" />{" "}
+          </div>
+          <div className="notfound-content">
+            <span>This page didn’t get the invite to <b>Prometeo</b>. But don't worry—<b>you did!</b> Let's get you back on <a href="/"><b><i>Home Page</i></b></a>.</span>
+          </div>
+        </div>
       </div>
-    </FadeIn>
+    </>
+    
+    
   );
 }
 
