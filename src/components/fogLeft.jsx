f1b9fe0357d4735957d43153b2a3b68298b98422
infernodragon456 <vighumandy420@gmail.com>
2024-10-30 12:03:27 +0530

    Add FogRight animations

--- src/components/fogLeft.jsx
+++ src/components/fogLeft.jsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import {easeIn, easeInOut, keyframes, motion} from "framer-motion"
+import { motion} from "framer-motion"
 const FogLeft = (props) => (
   <svg
     // width={883}
