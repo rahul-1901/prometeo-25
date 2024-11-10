cd0788288a7e6958580356a7b9126c79bb462222
infernodragon456 <vighumandy420@gmail.com>
2024-11-10 21:02:00 +0530

    Update file structure and add components for landing page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Canvas } from "@react-three/fiber";
+import { ScrollControls } from "@react-three/drei";
+import { usePlay } from "../components/landing/Play";
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
