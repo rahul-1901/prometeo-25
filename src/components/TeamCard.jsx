fbf273163419baa7ae533a8d414b0837f2047bcd
abhy <b23cs1085@iitj.ac.in>
2024-11-13 17:46:38 +0530

    added card hover effect

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -7,7 +7,7 @@ import {
 } from "react-icons/ai";
 import DefaultIMG from "../assets/defaultIMG.jpg";
 import "./TeamCard.css";
-import yellowImageBox from "../assets/team/Image_Box.png"
+import yellowImage from "../assets/team/yellowboxsvg.svg"
 import blueImageBox from "../assets/team/blueImagebox.png"
 const TeamCard = ({
   name,
@@ -19,14 +19,55 @@ const TeamCard = ({
   instagram,
   yellow
 }) => {
+  //TODO: how much from top
   return (
     <>
       <div className="wrapper ">
         {/* image */}
-        <div className="member-img" style={{ backgroundImage: `url(${imgURL})` }}>
-          {  
-            yellow%2!=0?(<img src={yellowImageBox} alt="" />):(<img src={blueImageBox} alt="" />)
-          }
+        <div className="membre ">
+          <div className="member-img " >
+            {  
+              yellow%2!=0?( <div className="background-overlay " style={{ backgroundImage: `url(${yellowImage})`}}   ></div>)
+              :( <div className="background-overlay " style={{ backgroundImage: `url(${blueImageBox})`}}   ></div>)
+            }
+            <img src={imgURL} alt="Image"/>
+           
+            
+          </div>
+          <div className="overly left">
+            <div className="middle">
+              <div className="social-icones">
+                {phone ? (
+                  <a href={`tel:${phone}`} className="icon">
+                    <AiOutlinePhone size={30} color="white" />
+                  </a>
+                ) : (
+                  <></>
+                )}
+                {email ? (
+                  <a href={`mailto:${email}`} className="icon">
+                    <AiOutlineMail size={30} color="white" />
+                  </a>
+                ) : (
+                  <></>
+                )}
+                {linkedin ? (
+                  <a href={linkedin} className="icon">
+                    <AiOutlineLinkedin size={30} color="white" />
+                  </a>
+                ) : (
+                  <></>
+                )}
+                {instagram ? (
+                  <a href={instagram} className="icon">
+                    <AiOutlineInstagram size={30} color="white" />
+                  </a>
+                ) : (
+                  <></>
+                )}
+              </div>
+            </div>
+            </div>
         </div>
         {/* text */}
         <div className="member-nm flex justify-center items-center w-full h-full">
