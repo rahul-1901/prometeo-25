e347d034b3b8a7f1dc1b1ea7e6399741b7432e20
Kritikaj2005 <kriti202jain@gmail.com>
2024-12-03 22:49:34 +0530

    accomodation

---
+++ src/pages/clgdesc.js
@@ -0,0 +1,71 @@
+import image_1 from "../assets/incentives/1.png";
+import image_2 from "../assets/incentives/2.png";
+import image_3 from "../assets/incentives/3.png";
+import image_6 from "../assets/incentives/6.png";
+import image_7 from "../assets/incentives/7.png";
+
+export const description = [
+  {
+    heading: "ABOUT US",
+    info: "Indian Institute of Technology Jodhpur was established in 2008, to foster technology, education, and research in India. The Institute is committed to pursuing technological advancements in the process of the economic develop ment of India. It functions from its sprawling residential Permanent Campus of 852 acres, with the campus plan being awarded a 5-star rating by GRIHA Council The institute's infrastructure is vividly unique while optimizing all resources to become a near-zero-emission campus. The Institute is committed to a mul tidisciplinary approach to technological development and it has established MoUs with six international universities, two international agencies, and three national institutes and universities for academic cooperation and research, The big breakthroughs in the research field include A1 intelligent gloves equipped with speech to facilitate specially-abled people. Hope to see you at Prometeo 2024. Please visit the Frequently Asked Questions (FAQ) section to get most of your queries resolved.",
+  },
+  {
+    heading: "ACCOMMODATION GUIDELINES",
+    info: `1. Every participant shall at all times be in possession of a current, government-issued photo ID and student participants must also carry a valid photo ID issued by their college. If a visitor cannot present their identification card, they will not be permitted to enter the Prometeo'24 campus.
+2. Alcohol, drugs, illegal substances, sharp objects, and any kind of explosives are absolutely prohibited on campus. Anything else that is deemed hazardous is prohibited. The Security and Prometeo'24 team's decision shall be final if there are any disputes.
+3. Throughout the fest of Prometeo '24, no outside vehicles will be permitted on campus.
+4. All participants are expected to maintain the campus's decorum and cleanliness as well as to diligently and consistently abide by its rules and regulations.
+5. IIT Jodhpur and its students disclaim all liability for any accidents that may or may not happen during the course of Prometeo '24.
+6. To prevent any unauthorized activities on campus, random checks would be conducted.
+7. Any commodities issued to the guests would have to be returned in sound condition to the organizers during check-out.
+8. Anyone found visiting a hostel other than the one that is designated will be penalized and appropriate action will be taken against them.
+9. Hostel guests must take care to prevent damage to the institute's property and facilities, since those found guilty of such behavior will be responsible for covering any necessary repairs.
+10. The administration will not be held liable for any damage or loss of property or valuables of the participants stored in places of lodging.
+11. Participants are requested to refrain from causing any disruptions to their other participants.
+12. Institute will provide the pickup and drop facility to the participants between bus/railway stations and campus during the dates which will be announced soon.`
+},
+
+
+];
+
+export const silver_amabassador = [
+  {
+    content: "Free Accommodation and Pronite Pass",
+    img: image_1,
+  },
+  {
+    content: "Certificate",
+    img: image_2,
+  },
+  {
+    content: "Free Entry to 1 Workshop",
+    img: image_3,
+  },
+  {
+    content: "Networking with IITians & LinkedIn endorsements",
+    img: image_7,
+  },
+];
+
+export const gold_amabassador = [
+  {
+    content: "Free Accommodation and Pronite Pass",
+    img: image_1,
+  },
+  {
+    content: "Certificate",
+    img: image_2,
+  },
+  {
+    content: "Goodies",
+    img: image_6,
+  },
+  {
+    content: "Free Entry to 2 Workshops",
+    img: image_3,
+  },
+  {
+    content: "Networking with IITians & LinkedIn endorsements",
+    img: image_7,
+  },
+];
