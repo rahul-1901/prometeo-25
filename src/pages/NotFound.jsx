26d4cb9214023c8c935b4a520b5ba89d787df30c
rahul-1901 <iitdelhi98@gmail.com>
2025-01-04 13:02:20 +0530

    notfoundPage

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,27 +3,17 @@ import { Link } from "react-router-dom";
 import "./NotFound.css";
 import sadPenguinImage from "../assets/sad-penguin.webp";
 import { HOME_PAGE_URL } from "../config";
+import pageNot from "../assets/pageNot.png";
 
 const NotFound = () => {
   return (
     <div className="notfound">
       <div className="notFound-container">
-        <h1>404 - Page Not Found</h1>
-        <img src={sadPenguinImage} alt="Sad Penguin" />
+        <img className="" src={pageNot}></img>
       </div>
-      <h3
-        style={{
-          marginTop: "10px",
-          fontWeight: "300",
-          fontFamily: "Share Tech, sans-serif",
-        }}
-      >
-        Sorry, the page you're looking for seems to be missing or the link is
-        incorrect.
+      <h3>
+        This page didn't get the invite to <b>Prometeo</b>. But don't worry—<b>you did!</b> Let's get you back on <Link to="/"><b>Home Page</b></Link>
       </h3>
-      <Link to={HOME_PAGE_URL}>
-        <button className="notFound-back-btn">Go Back to Home</button>
-      </Link>
     </div>
   );
 };
