fc37ca4280b2adab105534aa894c9b60aa10a9e8
rahul-1901 <iitdelhi98@gmail.com>
2025-01-11 18:25:50 +0530

    ruleBook changes

--- src/pages/PreEvents.jsx
+++ src/pages/PreEvents.jsx
@@ -15,7 +15,7 @@ function createEntry(e) {
         eventButton={e.prize ? e.prize : "Coming soon..."}
         workshop={e.type === "informal"}
         description={e.description}
-        rule={e.rulebook || e.rulebook_url || "no ruleBook"}
+        rule={e.rulebook ? e.rulebook : e.rulebook_url}
       />
     </FadeInContent>
   );
